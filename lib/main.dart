a1179f34126f55c0b756536bffb8a4d226a278d6
Yasser Ahmed <87115659+yasser-hammouda@users.noreply.github.com>
2024-07-26 14:03:39 +0300

    phase 2 firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do_list/home_page.dart';
@@ -5,7 +8,17 @@ import 'package:to_do_list/my_theme_data.dart';
 import 'package:to_do_list/providers/app_config_provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main (){
+void main () async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid ?
+  await Firebase.initializeApp(
+    options :const FirebaseOptions(apiKey: 'AIzaSyDFe1HxVMMvuaClaqwQlUv1u1zzBJPKMfE', // = current_key in Json
+        appId: 'com.example.to_do_list',  //package_name in Json
+        messagingSenderId: '590074684851', // project_number in Json
+        projectId: 'to-do-list-app-77278' //project_id in Json
+    )
+  ):
+  await Firebase.initializeApp ();
   runApp(ChangeNotifierProvider(
       create: (BuildContext context) =>AppConfigProvider(),
       child: MyApp()));
